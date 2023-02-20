3294961139732208c85635b5391607389602ecea
EC2 Default User <ec2-user@ip-172-31-27-127.ap-northeast-1.compute.internal>
2023-02-20 08:28:57 +0000

    first submit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+   root :to =>'homes#top' 
+
+  get '/books/index', to: 'books#index'
+  resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
